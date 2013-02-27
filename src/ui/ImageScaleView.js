db0025ba783b6bd45fa4b4e2cd884bb1ce0f8415
Mario Balibrera <mario@gameclosure.com>
2013-02-27 13:53:10 -0800

    adjusted logic to preserve examples

--- src/ui/ImageScaleView.js
+++ src/ui/ImageScaleView.js
@@ -142,67 +142,67 @@ exports = Class(ui.View, function (supr) {
 		var sw, sh, iw, ih, bounds;
 		opts = merge(opts, this._opts);
 
-		if (opts.autoSize) {
+		if (typeof img == 'string') {
+			bounds = GCResources.getMap()[img];
+			if (bounds) {
+				iw = bounds.w;
+				ih = bounds.h;
+			}
+		} else if (img instanceof Image && img.isLoaded()) {
+			bounds = img.getBounds();
+			iw = bounds.width;
+			ih = bounds.height;
+		}
+
+		if (!bounds) {
 			if (typeof img == 'string') {
-				bounds = GCResources.getMap()[img];
-				if (bounds) {
-					iw = bounds.w;
-					ih = bounds.h;
-				}
-			} else if (img instanceof Image && img.isLoaded()) {
-				bounds = img.getBounds();
-				iw = bounds.width;
-				ih = bounds.height;
+				img = new Image({url: img});
 			}
+			return img.doOnLoad(this, 'setImage', img, opts);
+		}
 
-			if (!bounds) {
-				if (typeof img == 'string') {
-					img = new Image({url: img});
+		if (opts.autoSize && this._scaleMethod == "stretch" && !((opts.width || opts.layoutWidth) && (opts.height || opts.layoutHeight))) {
+			autoSized = true;
+			if (this.style.fixedAspectRatio) {
+				this.style.updateAspectRatio(iw, ih);
+				var parent = this.getSuperview();
+				if (opts.width) {
+					iw = opts.width;
+					ih = opts.width / this.style.aspectRatio;
 				}
-				return img.doOnLoad(this, 'setImage', img, opts);
+				else if (opts.height) {
+					ih = opts.height;
+					iw = opts.height * this.style.aspectRatio;
+				}
+				else if (opts.layoutWidth && parent.style.width) {
+					iw = parent.style.width * parseFloat(opts.layoutWidth) / 100;
+					ih = iw / this.style.aspectRatio;
+				}
+				else if (opts.layoutHeight && parent.style.height) {
+					ih = parent.style.height * parseFloat(opts.layoutHeight) / 100;
+					iw = ih * this.style.aspectRatio;
+				}
 			}
+			this.style.width = iw;
+			this.style.height = ih;
+		}
 
-			if (this._scaleMethod == "stretch" && !((opts.width || opts.layoutWidth) && (opts.height || opts.layoutHeight))) {
-				autoSized = true;
-				if (this.style.fixedAspectRatio) {
-					this.style.updateAspectRatio(iw, ih);
-					var parent = this.getSuperview();
-					if (opts.width) {
-						iw = opts.width;
-						ih = opts.width / this.style.aspectRatio;
-					}
-					else if (opts.height) {
-						ih = opts.height;
-						iw = opts.height * this.style.aspectRatio;
-					}
-					else if (opts.layoutWidth && parent.style.width) {
-						iw = parent.style.width * parseFloat(opts.layoutWidth) / 100;
-						ih = iw / this.style.aspectRatio;
-					}
-					else if (opts.layoutHeight && parent.style.height) {
-						ih = parent.style.height * parseFloat(opts.layoutHeight) / 100;
-						iw = ih * this.style.aspectRatio;
-					}
-				}
-				this.style.width = iw;
-				this.style.height = ih;
-			} else if (this._isSlice) {
-				var sourceSlicesHor = this._sourceSlicesHor;
-				var sourceSlicesVer = this._sourceSlicesVer;
-				if (sourceSlicesHor) {
-					sw = sourceSlicesHor[0] + sourceSlicesHor[1] + sourceSlicesHor[2];
-					var scale = iw / sw;
-					sourceSlicesHor[0] *= scale;
-					sourceSlicesHor[1] *= scale;
-					sourceSlicesHor[2] *= scale;
-				}
-				if (sourceSlicesVer) {
-					sh = sourceSlicesVer[0] + sourceSlicesVer[1] + sourceSlicesVer[2];
-					var scale = ih / sh;
-					sourceSlicesVer[0] *= scale;
-					sourceSlicesVer[1] *= scale;
-					sourceSlicesVer[2] *= scale;
-				}
+		if (this._isSlice) {
+			var sourceSlicesHor = this._sourceSlicesHor;
+			var sourceSlicesVer = this._sourceSlicesVer;
+			if (sourceSlicesHor) {
+				sw = sourceSlicesHor[0] + sourceSlicesHor[1] + sourceSlicesHor[2];
+				var scale = iw / sw;
+				sourceSlicesHor[0] *= scale;
+				sourceSlicesHor[1] *= scale;
+				sourceSlicesHor[2] *= scale;
+			}
+			if (sourceSlicesVer) {
+				sh = sourceSlicesVer[0] + sourceSlicesVer[1] + sourceSlicesVer[2];
+				var scale = ih / sh;
+				sourceSlicesVer[0] *= scale;
+				sourceSlicesVer[1] *= scale;
+				sourceSlicesVer[2] *= scale;
 			}
 		}
 
