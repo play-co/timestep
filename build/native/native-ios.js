cb327fdb121ba21d7e6ceade65c9fde7c72fa713
Chris A. Taylor <chris@gameclosure.com>
2013-03-05 19:22:19 -0800

    Use common track

--- build/native/native-ios.js
+++ build/native/native-ios.js
@@ -799,11 +799,7 @@ exports.package = function (builder, project, opts, next) {
 	var metadata = opts.metadata;
 	var servicesURL = opts.servicesURL;
 
-	// -- MixPanel Analytics: Improve DevKit by sharing anonymous statistics
-	var MixPanel = require('mixpanel');
-	var myMixPanel = MixPanel && MixPanel.init("08144f9200265117af1ba86e226c352a");
-	myMixPanel && myMixPanel.track("BasilBuildNativeIOS", {"version":common.sdkVersion.src, "clean":clean, "debug":debug, "compress":opts.compress});
-	// -- End of Analytics
+	common.track("BasilBuildNativeIOS", {"clean":clean, "debug":debug, "compress":opts.compress});
 
 	getTealeafIOSPath(function(dir) {
 		iOSPath = dir;
