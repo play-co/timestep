d8b68ea80e1a2cf975d66da1b230b6eab01a5512
Chris A. Taylor <chris@gameclosure.com>
2013-03-13 19:38:24 -0700

    Add --install parameter

--- build/native/native-android.js
+++ build/native/native-android.js
@@ -614,6 +614,7 @@ exports.package = function (builder, project, opts, next) {
 
 	var argParser = require('optimist')
 		.alias('help', 'h').describe('help', 'Display this help menu')
+		.alias('install', 'i').describe('install', 'Launch `adb install` after build completes').boolean('install').default('install', false)
 		.alias('debug', 'd').describe('debug', 'Create debug build').boolean('debug').default('debug', opts.template !== "release")
 		.alias('clean', 'c').describe('clean', 'Clean build before compilation').boolean('clean').default('clean', opts.template !== "debug");
 	var argv = argParser.argv;
@@ -699,10 +700,11 @@ exports.package = function (builder, project, opts, next) {
 		opts.output = path.join(destDir, "assets/resources");
 
 		// Parallelize android project setup and sprite building.
+		var apkPath;
 		var f = ff(function () {
 			_builder.common.child("node", [path.join(androidDir, "plugins/installPlugins.js")], {}, f.wait());
 			require('./native').writeNativeResources(project, opts, f.waitPlain());
-			
+		
 			makeAndroidProject(project, packageName, activity, title, appID,
 					shortName, opts.version, debug, destDir, servicesURL, metadata,
 					studioName, f.waitPlain());
@@ -718,6 +720,8 @@ exports.package = function (builder, project, opts, next) {
 			
 			copySplash(project, destDir, f());
 		}, function () {
+			var onDoneBuilding = f();
+
 			buildAndroidProject(destDir, debug, function (success) {
 				//if (!success) {
 				//  logger.error("BUILD FAILED");
@@ -732,7 +736,7 @@ exports.package = function (builder, project, opts, next) {
 				}
 
 				(!debug ? signAPK : nextStep)(shortName, destDir, function () {
-					var apkPath = path.join(apkDir, shortName + ".apk");
+					apkPath = path.join(apkDir, shortName + ".apk");
 					if (fs.existsSync(apkPath)) {
 						fs.unlinkSync(apkPath);
 					}
@@ -743,7 +747,7 @@ exports.package = function (builder, project, opts, next) {
 						_builder.common.copyFileSync(destApkPath, apkPath);
 						logger.log("built", clc.yellow.bright(packageName));
 						logger.log("saved to " + clc.blue.bright(apkPath));
-						next(0);
+						onDoneBuilding();
 					} else {
 						logger.error(clc.red.bright("NO FILE AT " + destApkPath));
 						next(2);
@@ -751,6 +755,20 @@ exports.package = function (builder, project, opts, next) {
 					
 				});
 			});
+		}, function () {
+			if (argv.install) {
+				var cmd = 'adb uninstall "' + packageName + '"';
+				logger.log('Install: Running ' + cmd + '...');
+				_builder.common.child('adb', ['uninstall', packageName], {}, f.waitPlain()); //this is waitPlain because it can fail and not break.
+			}
+		}, function () {
+			if (argv.install) {
+				var cmd = 'adb install -r "' + apkPath + '"';
+				logger.log('Install: Running ' + cmd + '...');
+				_builder.common.child('adb', ['install', '-r', apkPath], {}, f.waitPlain()); //this is waitPlain because it can fail and not break.
+			}
+		}, function () {
+			next(0);
 		}).error(function (err) {
 			console.error("unexpected error:");
 			console.error(err);
