3971686ff44a89475d94c989c45ca608f37be8ee
Mario Balibrera <mario@gameclosure.com>
2013-05-17 11:57:43 -0700

    added license

--- src/ui/ScoreView.js
+++ src/ui/ScoreView.js
@@ -1,11 +1,31 @@
+/**
+ * @license
+ * This file is part of the Game Closure SDK.
+ *
+ * The Game Closure SDK is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * The Game Closure SDK is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with the Game Closure SDK.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * package ui.ScoreView;
+ *
+ * This class is designed for high performance text rendering using images.
+ * It is ideal for scores or other in-game counters that update often.
+ */
+
 import ui.View as View;
 import ui.ImageView as ImageView;
 import ui.resource.Image as Image;
-
-/* ScoreView.js
- * This class is designed for high performance text-rendering using images.
- * It is ideal for scores or other in-game counters that update often.
- */
 
 exports = Class(View, function(supr) {
 
