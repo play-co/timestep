c769fd750ccf17f11d1b79fe7328eb239dbfe5ae
Mario Balibrera <mario@gameclosure.com>
2013-06-07 13:07:06 -0700

    allow flex 0 for views hidden in linear layouts

--- src/ui/layout/LinearLayout.js
+++ src/ui/layout/LinearLayout.js
@@ -317,7 +317,7 @@ exports = Class(BoxLayout, function (supr) {
 
 			v.margins = (s[propA] || 0) + (s[propB] || 0);
 
-			if (s.flex) {
+			if (s.flex || s.flex == 0) {
 				flexSum += s.flex;
 				v.baseSize = s[minPropDim] || 0;
 				sum += v.baseSize;
