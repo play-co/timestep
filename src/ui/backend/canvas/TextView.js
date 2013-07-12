b0751cfebc3bc1eebb5e13a29fb21ea69dbfa3d1
Martin Hunt <martin@gameclosure.com>
2013-07-12 12:07:44 -0700

    re-disable textview buffer since positioning is off

--- src/ui/backend/canvas/TextView.js
+++ src/ui/backend/canvas/TextView.js
@@ -67,7 +67,7 @@ var TextView = exports = Class(View, function (supr) {
 		horizontalAlign: "center",
 
 		// misc properties...
-		buffer: GLOBAL.NATIVE && !device.simulatingMobileNative,
+		buffer: false, // GLOBAL.NATIVE && !device.simulatingMobileNative,
 		backgroundColor: null
 	};
 
