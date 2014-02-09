1627d24a0fe61b5a73501493bf472b4f5bff5016
Martin Hunt <martin@gameclosure.com>
2014-02-09 07:47:06 -0800

    no dynamic code loading in native env

--- build/native/env.js
+++ build/native/env.js
@@ -5,33 +5,26 @@
 ;(function() {
 	function getCwd() { return NATIVE.location.substring(0, NATIVE.location.lastIndexOf('/') + 1) + 'code/__cmd__/'; }
 	NATIVE.console.log(NATIVE.location);
-	
+
 	var mobileEnv = function(util) {
 		var SLICE = Array.prototype.slice,
 			cwd = null;
-				
+
 		this.name = /android/.test(GLOBAL.userAgent) ? 'android' : 'ios';
-	
+
 		this.global = GLOBAL;
 		this.global.jsio = jsio;
-	
+
 		this.log = function() { NATIVE.console.log(SLICE.call(arguments).join(' ')); }
 		this.getCwd = getCwd;
 		this.getPath = function() { return './sdk/jsio/'; };
 		this.eval = function(code, path) { return NATIVE.eval(code, path); };
-		this.fetch = function(filePath) {
-			if (filePath.substring(0, 4) != 'http') {
-				var parts = this.getCwd().match(/^(https?):\/\/(.*)$/);
-				this.log('cwd is ' + this.getCwd());
-				filePath = parts[1] + '://' + util.resolveRelativePath(parts[2] + filePath);
-			}
-			return NATIVE.getFileSync(filePath);
-		}
+		this.fetch = function(filePath) { return false; }
 	}
-	
+
 //TODO detect which name to pass
 	jsio.setEnv(mobileEnv);
-	
+
 	// jsio.path.add('./sdk');
 	// jsio.path.add('./sdk/gc/api');
 	// jsio.path.add('./sdk/lib');
