a6b5a3a85510791f853e458361e679f724720739
Raymond Cook <raymond@weeby.co>
2015-09-11 13:01:50 -0700

    Alpha blending support

--- src/platforms/browser/WebGLContext2D.js
+++ src/platforms/browser/WebGLContext2D.js
@@ -21,6 +21,7 @@
  */
 
 import device;
+import .FontRenderer;
 
 exports = Class(function() {
 
@@ -37,13 +38,15 @@ exports = Class(function() {
 		this._canvasElement.height = opts.height || device.height;
 		this.font = '11px ' + device.defaultFontFamily;
 
+		this.globalAlpha = 1;
+
 		var gl = this.ctx = this._canvasElement.getContext('webgl');
 		gl.clearColor(0.0, 0.0, 0.0, 1.0);
 		gl.disable(gl.DEPTH_TEST);
 		gl.disable(gl.CULL_FACE);
 		gl.enable(gl.BLEND);
 
-		this._vertexCache = new ArrayBuffer(16 * 4);
+		this._vertexCache = new ArrayBuffer(32 * 4);
 		this._verticies = new Float32Array(this._vertexCache);
 		this._indexCache = new Uint16Array(6);
 		this._indexCache[0] = 0;
@@ -61,6 +64,9 @@ exports = Class(function() {
 
 		this._transform = { a: 1, b: 0, c: 0, d: 1, tx: 0, ty: 0 };
 		this.textureCache = [];
+
+		this._lastTextureId = -1;
+		this._batchIndex = 0;
 
 		// this.ctx.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 	};
@@ -80,11 +86,15 @@ exports = Class(function() {
 
 	    var positionIndex = gl.getAttribLocation(this._shaderProgram, "aPosition");
 	    var uvIndex = gl.getAttribLocation(this._shaderProgram, "aTextureCoord");
+	    var colorIndex = gl.getAttribLocation(this._shaderProgram, "aColor");
 
-		gl.vertexAttribPointer(positionIndex, 2, gl.FLOAT, false, 16, 0);
-		gl.vertexAttribPointer(uvIndex, 2, gl.FLOAT, false, 16, 8);
-		gl.enableVertexAttribArray(0);
-		gl.enableVertexAttribArray(1);
+		gl.vertexAttribPointer(positionIndex, 2, gl.FLOAT, false, 32, 0);
+		gl.vertexAttribPointer(uvIndex, 2, gl.FLOAT, false, 32, 8);
+		gl.vertexAttribPointer(colorIndex, 4, gl.FLOAT, false, 32, 16);
+
+		gl.enableVertexAttribArray(positionIndex);
+		gl.enableVertexAttribArray(uvIndex);
+		gl.enableVertexAttribArray(colorIndex);
 	};
 
 	this._initializeShaders = function() {
@@ -105,6 +115,8 @@ exports = Class(function() {
 
 		var resolutionLocation = gl.getUniformLocation(this._shaderProgram, "uResolution");
 		gl.uniform2f(resolutionLocation, this._canvasElement.width, this._canvasElement.height);
+
+		gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
 	};
 
 	this.createVertexShader = function() {
@@ -112,11 +124,14 @@ exports = Class(function() {
 		var src = [
 			'attribute vec2 aTextureCoord;',
 			'attribute vec2 aPosition;',
+			'attribute vec4 aColor;',
 			'uniform vec2 uResolution;',
-			'varying highp vec2 vTextureCoord;',
+			'varying vec2 vTextureCoord;',
+			'varying vec4 vColor;',
 			'void main() {',
 			'	vTextureCoord = aTextureCoord;',
 			'	vec2 clipSpace = (aPosition / uResolution) * 2.0 - 1.0;',
+			'	vColor = aColor;',
 			'	gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);',
 			'}'
 		].join("\n");
@@ -132,10 +147,11 @@ exports = Class(function() {
 		var gl = this.ctx;
 		var src = [
 		    'precision mediump float;',
-			'varying highp vec2 vTextureCoord;',
+			'varying vec2 vTextureCoord;',
+			'varying vec4 vColor;',
 			'uniform sampler2D uSampler;',
 		    'void main(void) {',
-		  	'  gl_FragColor = texture2D(uSampler, vec2(vTextureCoord.s, vTextureCoord.t));',
+		  	'  gl_FragColor = texture2D(uSampler, vTextureCoord) * vColor;',
 		    '}'
 		].join("\n");
 
@@ -194,47 +210,34 @@ exports = Class(function() {
 		var vc = this._verticies;
 		var dxW = dx + dWidth;
 		var dyH = dy + dHeight;
-
-		m = this._transform;
-		var x0 = dx * m.a + dy * m.c + m.tx;
-		var y0 = dx * m.b + dy * m.d + m.ty;
-		var x1 = dxW * m.a + dy * m.c + m.tx;
-		var y1 = dxW * m.b + dy * m.d + m.ty;
-		var x2 = dx * m.a + dyH * m.c + m.tx;
-		var y2 = dx * m.b + dyH * m.d + m.ty;
-		var x3 = dxW * m.a + dyH * m.c + m.tx;
-		var y3 = dxW * m.b + dyH * m.d + m.ty;
-
+		var m = this._transform;
 		var tw = image.width;
 		var th = image.height;
-		var u0 = sx / tw;
-		var v0 = sy / th;
-		var u1 = (sx + sWidth) / tw;
-		var v1 = v0;
-		var u2 = u0;
-		var v2 = (sy + sHeight) / th;
-		var u3 = u1;
-		var v3 = v2;
 
-		vc[0] = x0;
-		vc[1] = y0;
-		vc[2] = u0;
-		vc[3] = v0;
+		vc[0] = dx * m.a + dy * m.c + m.tx; // x0
+		vc[1] = dx * m.b + dy * m.d + m.ty; // y0
+		vc[2] = sx / tw; // u0
+		vc[3] = sy / th; // v0
 
-		vc[4] = x1;
-		vc[5] = y1;
-		vc[6] = u1;
-		vc[7] = v1;
+		vc[8] = dxW * m.a + dy * m.c + m.tx; // x1
+		vc[9] = dxW * m.b + dy * m.d + m.ty; // y1
+		vc[10] = (sx + sWidth) / tw; // u1
+		vc[11] = vc[3]; // v1
 
-		vc[8] = x2;
-		vc[9] = y2;
-		vc[10] = u2;
-		vc[11] = v2;
+		vc[16] = dx * m.a + dyH * m.c + m.tx; // x2
+		vc[17] = dx * m.b + dyH * m.d + m.ty; // y2
+		vc[18] = vc[2]; // u2
+		vc[19] = (sy + sHeight) / th;  // v2
 
-		vc[12] = x3;
-		vc[13] = y3;
-		vc[14] = u3;
-		vc[15] = v3;
+		vc[24] = dxW * m.a + dyH * m.c + m.tx; // x3
+		vc[25] = dxW * m.b + dyH * m.d + m.ty; // y3
+		vc[26] = vc[10]; // u4
+		vc[27] = vc[19]; // v4
+
+		vc[4] = vc[12] = vc[20] = vc[28] = 1.0; // R
+		vc[5] = vc[13] = vc[21] = vc[29] = 1.0; // G
+		vc[6] = vc[14] = vc[22] = vc[30] = 1.0; // B
+		vc[7] = vc[15] = vc[23] = vc[31] = this.globalAlpha; // A
 
 		gl.bufferData(gl.ARRAY_BUFFER, this._vertexCache, gl.DYNAMIC_DRAW);
 		gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
@@ -271,5 +274,7 @@ exports = Class(function() {
 		m.tx = tx;
 		m.ty = ty;
 	};
+
+	this.measureText = FontRenderer.wrapMeasureText;
 
 });
