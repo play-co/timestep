a4cdd8c2af2a15d2343fa40f2f1e599c912a932a
Raymond Cook <raymond@weeby.co>
2015-10-29 12:16:07 -0700

    Fixing issue with BlendEngine not rendering on iOS

--- src/ui/BlendEngine.js
+++ src/ui/BlendEngine.js
@@ -31,7 +31,7 @@ var sin = Math.sin;
 var cos = Math.cos;
 var max = Math.max;
 var min = Math.min;
-var MIN_VALUE = Number.MIN_VALUE;
+var MIN_VALUE = -Number.MAX_VALUE;
 var MAX_VALUE = Number.MAX_VALUE;
 
 // class-wide image cache
@@ -138,7 +138,6 @@ exports = Class(View, function(supr) {
 		this._canvW = 1;
 		this._canvH = 1;
 		this._canvas = new Canvas({ width: MAX_TEX_WIDTH, height: MAX_TEX_HEIGHT, useWebGL: true });
-		this._img = new Image({ srcImage: this._canvas });
 	};
 
 	this.obtainParticleArray = function(count) {
@@ -382,6 +381,17 @@ exports = Class(View, function(supr) {
 
 		// establish canvas size and position, bounded by max texture size
 		if (shouldUpdate) {
+
+			if (minX > maxX) {
+				minX = 0;
+				maxX = MAX_TEX_WIDTH;
+			}
+
+			if (minY > maxY) {
+				minY = 0;
+				maxY = MAX_TEX_HEIGHT;
+			}
+
 			var canvX = minX;
 			var canvY = minY;
 			var canvW = maxX - minX;
@@ -396,8 +406,6 @@ exports = Class(View, function(supr) {
 				canvY = cy - MAX_TEX_HEIGHT / 2;
 				canvH = MAX_TEX_HEIGHT;
 			}
-			this._img.setSourceWidth(canvW);
-			this._img.setSourceHeight(canvH);
 
 			// render our particle images to the canvas's context
 			var ctx = this._canvas.getContext("2D");
@@ -451,7 +459,7 @@ exports = Class(View, function(supr) {
 	};
 
 	this.render = function(ctx) {
-		this._img.render(ctx, this._canvX, this._canvY, this._canvW, this._canvH);
+		ctx.drawImage(this._canvas, 0, 0, this._canvW, this._canvH, this._canvX, this._canvY, this._canvW, this._canvH);
 	};
 
 	this.getActiveParticles = function() {
