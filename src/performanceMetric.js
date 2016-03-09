d37fc3b716f5da371456ead2b9acf016357a699e
Jack <jack@Weebys-MacBook-Pro-2.local>
2016-03-09 12:58:07 -0800

    indentation change

--- src/performanceMetric.js
+++ src/performanceMetric.js
@@ -27,92 +27,98 @@ var floor = Math.floor;
 var DEFAULT_RANK = 0;
 var DEFAULT_ALLOW_REDUCTION = true;
 
-	
+  
 var PerformanceTester = Class(function () {
-	this.historySize = 200;
-	var _lastTick = Date.now();
-	var _history = [];
-	var historyIndex = 0;
+  this.historySize = 200;
+  var _lastTick = Date.now();
+  var _history = [];
+  var historyIndex = 0;
+  var runningSum = 0;
 
-	this.init = function () {
-		this.LOWER_BOUND = 10;
-		this.HIGHER_BOUND = 60;
-		this.measuring = false;
-	};
+  this.init = function () {
+    this.LOWER_BOUND = 10;
+    this.HIGHER_BOUND = 60;
+    this.measuring = false;
+    this.performanceScore = 0;
+  };
 
-	this.startMeasuring = function() {
-		if (!this.measuring) {
-			this.measuring = true;
-			if (engine === null) {
-				import ui.Engine as Engine;
-				engine = Engine.get();
-			}
-			_lastTick = Date.now();
-			engine.subscribe('Tick', this, 'onTick');
-		}
-	};
+  this.startMeasuring = function() {
+    if (!this.measuring) {
+      this.measuring = true;
+      if (engine === null) {
+        import ui.Engine as Engine;
+        engine = Engine.get();
+      }
+      _lastTick = Date.now();
+      engine.subscribe('Tick', this, 'onTick');
+    }
+  };
 
-	this.stopMeasuring = function() {
-		if (this.measuring) {
-		  this.measuring = false;
-			engine.unsubscribe('Tick', this, 'onTick');
-		}
-	};
+  this.stopMeasuring = function() {
+    if (this.measuring) {
+      this.measuring = false;
+      engine.unsubscribe('Tick', this, 'onTick');
+    }
+  };
 
-	this.onTick = function(dt) {
-		var now = Date.now();
-		var delta = now - _lastTick;
-		_history[historyIndex++] = delta;
-		if (historyIndex >= this.historySize) {
-			historyIndex = 0;
-		}
-		this.getPerformanceScore();
-		_lastTick = now;
-	};
+  this.onTick = function(dt) {
+    var now = Date.now();
+    var delta = now - _lastTick;
+    if (_history[historyIndex]) {
+      runningSum -= _history[historyIndex];
+    }
+    runningSum += delta;
+    _history[historyIndex++] = delta;
+    this.performanceScore = runningSum / this.historySize;
+    if (historyIndex >= this.historySize) {
+      historyIndex = 0;
+    }
+    _lastTick = now;
+  };
 
-	this.getAverageTicksSpeed = function() {
-		var result = 0;
-		for (var i = 0; i < _history.length; i++) {
-			result += _history[i];
-		}
-		result /= _history.length;
-		return result;
-	};
+  this.getAverageTicksSpeed = function() {
+    var result = 0;
+    for (var i = 0; i < _history.length; i++) {
+      result += _history[i];
+    }
+    result /= _history.length;
+    return result;
+  };
 
-	this.getPerformanceScore = function() {
-		var tickSpeed = this.getAverageTicksSpeed();
-		var ticksPerSecond = 1000 / tickSpeed;
-		var adjustedTicksPerSecond = Math.min(ticksPerSecond, 60);
-		var mappedScore = _map(adjustedTicksPerSecond, this.LOWER_BOUND, this.HIGHER_BOUND, 0, 100);
-		return Math.max(0, mappedScore);
-	}
-	
-	this.getParticleCount = function(count, performance) {
-		if (!performance) {return count;}
+  this.getPerformanceScore = function() {
+    var tickSpeed = this.getAverageTicksSpeed();
+    var ticksPerSecond = 1000 / tickSpeed;
+    var adjustedTicksPerSecond = Math.min(ticksPerSecond, 60);
+    var mappedScore = _map(adjustedTicksPerSecond, this.LOWER_BOUND, this.HIGHER_BOUND, 0, 100);
+    return Math.max(0, mappedScore);
+  }
+  
+  this.getParticleCount = function(count, performance) {
+    if (!performance) {return count;}
 
-		var currCount = count;
-		var mR = 50;
-		var pR = performance.effectPerformanceRank || DEFAULT_RANK;
-		var aR = (typeof performance.allowReduction !== 'undefined')
-			? performance.allowReduction
-			: DEFAULT_ALLOW_REDUCTION;
+    var currCount = count;
+    var mR = 50;
+    var pR = performance.effectPerformanceRank || DEFAULT_RANK;
+    var aR = (typeof performance.allowReduction !== 'undefined')
+      ? performance.allowReduction
+      : DEFAULT_ALLOW_REDUCTION;
 
-		if (mR < pR) {
-			currCount = (aR)
-				? count * mR / pR
-				: 0;  
-		}	
+    if (mR < pR) {
+      currCount = (aR)
+        ? count * mR / pR
+        : 0;  
+    } 
 
-		return currCount;
-	};	
+    return currCount;
+  };  
 
-	var _map = function(val, start1, stop1, start2, stop2) {
-		var range1 = stop1 - start1;
-		var range2 = stop2 - start2;
-		var valPosition = val - start1;
-		var valuePercentage = valPosition / range1;
-		return start2 + range2 * valuePercentage;
-	};
+  var _map = function(val, start1, stop1, start2, stop2) {
+    var range1 = stop1 - start1;
+    var range2 = stop2 - start2;
+    var valPosition = val - start1;
+    var valuePercentage = valPosition / range1;
+    return start2 + range2 * valuePercentage;
+  };
 });
 
 exports = new PerformanceTester();
