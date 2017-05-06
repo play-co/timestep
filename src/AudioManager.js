f829ff9ac6c8f0c6f6c8457f8d4fec80239b4007
Jimmy Griffith <rogueSkib@gmail.com>
2017-05-05 21:52:58 -0700

    removed some old broken code

--- src/AudioManager.js
+++ src/AudioManager.js
@@ -153,13 +153,6 @@ class MultiSound {
     var loop = this.loop;
     var volume = opts.volume !== undefined ? opts.volume : 1;
 
-    // HTML5 hack for browsers
-    var isPaused = bind(this, 'isPaused');
-    var _checkPauseOnPlay = function (src) {
-      this.removeEventListener(arguments.callee);
-      isPaused() && this.pause();
-    };
-
     if (_ctx) {
       this._useAudioContext = true;
       this._loader = soundManager.getAudioLoader();
@@ -185,15 +178,8 @@ class MultiSound {
           audio.volume = volume;
           audio.isBackgroundMusic = opts.background;
           audio.src = fullPath;
-          audio.preload = audio.readyState !== 4 || soundManager._preload &&
-            !opts.background ? 'auto' : 'none';
-
-          // If you pause or mute an html5 audio object in the browser
-          // before the sound is ready, it will play anyway.  Here, we
-          // check the pause status when the audio starts playing.
-          if (audio.addEventListener) {
-            audio.addEventListener('playing', _checkPauseOnPlay);
-          }
+          audio.preload = audio.readyState !== 4
+            || soundManager._preload && !opts.background ? 'auto' : 'none';
 
           sources.push(audio);
           if (audio.isBackgroundMusic && NATIVE && NATIVE.sound) {
